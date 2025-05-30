66f603fd6dd3c3507e03bef137ef21da01964db5
VictorHeDev <VictorHe97@gmail.com>
2025-05-29 22:17:39 -0400

    Write a test to try to add sums of empty slices

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -37,3 +37,33 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
+
+func TestSumLastElements(t *testing.T) {
+	got := SumLastElements([]int{1, 2}, []int{0, 9})
+	want := 11
+
+	if got != want {
+		t.Errorf("got %v want %d", got, want)
+	}
+}
