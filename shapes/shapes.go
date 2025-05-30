9b357fb90b19bdba1243b05e3c25a495072ccd0a
VictorHeDev <VictorHe97@gmail.com>
2025-05-30 00:04:10 -0400

    Write the Area method for rectangles and circles

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -5,10 +5,18 @@ type Rectangle struct {
 	Height float64
 }
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+type Circle struct {
+	Radius float64
 }
 
-func Area(width, height float64) float64 {
-	return width * height
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return c.Radius * c.Radius * 3.141592653589793 // π * r^2
 }
