1e0de38901f7848c830f70d6904f387d3cdd9644
VictorHeDev <VictorHe97@gmail.com>
2025-05-30 00:14:49 -0400

    Create a Shape interface for rectangles and circles

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -1,5 +1,11 @@
 package shapes
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -18,5 +24,5 @@ func (r Rectangle) Area() float64 {
 }
 
 func (c Circle) Area() float64 {
-	return c.Radius * c.Radius * 3.141592653589793 // π * r^2
+	return c.Radius * c.Radius * math.Pi // π * r^2
 }
