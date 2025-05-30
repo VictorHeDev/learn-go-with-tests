d29041404f770f0b1a900e9e8b60742cb9684931
VictorHeDev <VictorHe97@gmail.com>
2025-05-30 00:32:24 -0400

    Finish with creating table driven tests for shapes

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -32,3 +32,23 @@ func TestArea(t *testing.T) {
 		checkArea(t, circle, 314.1592653589793) // π * r^2
 	})
 }
+
+func TestArea2(t *testing.T) {
+	// create an anonymous struct
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793}, // π * r^2
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.hasArea {
+			t.Errorf("%#v got %g hasArea %g", tt.shape, got, tt.hasArea)
+		}
+	}
+}
