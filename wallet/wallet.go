df8d91d806bfae9ec22795c28e2cbeb9f6f84a2c
VictorHeDev <VictorHe97@gmail.com>
2025-05-31 20:56:47 -0400

    Use global error for insufficient wallet balance

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -15,6 +15,9 @@ type Stringer interface {
 	String() string
 }
 
+// using var makes it a global variable
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
@@ -30,7 +33,7 @@ func (b Bitcoin) String() string {
 
 func (w *Wallet) Withdraw(amount Bitcoin) error {
 	if amount > w.balance {
-		return errors.New("cannot withdraw, insufficient funds")
+		return ErrInsufficientFunds
 	}
 	w.balance -= amount
 	return nil
