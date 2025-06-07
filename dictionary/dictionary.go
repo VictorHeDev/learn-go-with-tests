618169110608c41c66eacda1f111c6e3b20d5f1b
VictorHeDev <VictorHe97@gmail.com>
2025-06-07 02:02:49 -0400

    Check for existing word in Dictionary

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -5,6 +5,7 @@ import "errors"
 type Dictionary map[string]string
 
 var ErrNotFound = errors.New("could not find the word you were looking for")
+var ErrWordExists = errors.New("word already exists in the dictionary")
 
 // func Search(dictionary Dictionary, key string) string {
 // 	return dictionary[key]
@@ -15,5 +16,15 @@ func (d Dictionary) Search(key string) (string, error) {
 	if !ok {
 		return "", ErrNotFound
 	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(key, value string) (string, error) {
+	definition, ok := d[key]
+
+	if !ok {
+		d[key] = value
+	}
+
 	return definition, nil
 }
--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -26,6 +26,29 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is a test"
+
+		dictionary.Add(word, definition)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	// t.Run("existing word", func(t *testing.T) {
+	// 	word := "test"
+	// 	definition := "this is a test"
+
+	// 	dictionary[word] = definition
+	// 	err := dictionary.Add(word, "new definition")
+
+	// 	assertError(t, err, ErrWordExists)
+	// 	assertStrings(t, dictionary[word], definition)
+	// })
+}
+
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
@@ -38,4 +61,14 @@ func assertError(t testing.TB, got, want error) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+	assertStrings(t, got, definition)
 }
