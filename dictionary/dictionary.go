afcabda35cc2ea775277a32360cae66e40e2398b
VictorHeDev <VictorHe97@gmail.com>
2025-06-07 04:11:23 -0400

    Add test for update

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -4,8 +4,9 @@ type Dictionary map[string]string
 type DictionaryErr string
 
 const (
-	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists = DictionaryErr("word already exists in the dictionary")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("word already exists in the dictionary")
+	ErrWordDoesNotExist = DictionaryErr("word does not exist in the dictionary")
 )
 
 func (e DictionaryErr) Error() string {
@@ -36,5 +37,19 @@ func (d Dictionary) Add(key, value string) error {
 		return err
 	}
 
+	return nil
+}
+
+func (d Dictionary) Update(key, value string) error {
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[key] = value
+	default:
+		return err
+	}
 	return nil
 }
